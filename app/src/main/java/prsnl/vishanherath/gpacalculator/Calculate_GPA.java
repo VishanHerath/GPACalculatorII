99d49883b6bec10aa6cb52f71dafcb5134ab1562
Anuththara18 <anuththarajayakody18@gmail.com>
2020-07-19 14:37:23 +0530

    Button Added

--- app/src/main/java/prsnl/vishanherath/gpacalculator/Calculate_GPA.java
+++ app/src/main/java/prsnl/vishanherath/gpacalculator/Calculate_GPA.java
@@ -10,5 +10,7 @@ public class Calculate_GPA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate__g_p);
+
+
     }
 }
