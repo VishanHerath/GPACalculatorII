ad6c1da0396327d43ab8cb14c32e1bdd3f72024b
VishanHerath <vishanherath1998@gmail.com>
2020-07-19 14:42:57 +0530

    III

--- app/src/main/java/prsnl/vishanherath/gpacalculator/Calculate_GPA.java
+++ app/src/main/java/prsnl/vishanherath/gpacalculator/Calculate_GPA.java
@@ -2,7 +2,14 @@ package prsnl.vishanherath.gpacalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import android.widget.TextView;
 
 public class Calculate_GPA extends AppCompatActivity {
 
@@ -10,5 +17,69 @@ public class Calculate_GPA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate__g_p);
+
+        LinearLayout linLayout;
+        String NoFS;
+        int NoFI;
+
+        linLayout = findViewById(R.id.linearLayout);
+        EditText nof = (EditText) findViewById(R.id.NoFeditText);
+
+        NoFS = nof.getText().toString();
+        NoFI = Integer.parseInt(NoFS);
+
+
+        for(int i=0; i<NoFI; i++){
+            addTextView();
+        }
+
     }
+
+    private void addTextView() {
+        //Adding a LinearLayout with HORIZONTAL orientation
+        LinearLayout textLinearLayout = new LinearLayout(this);
+        textLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        textLinearLayout.addView(textLinearLayout);
+
+        for (int i = 1; i <= 3; i++) {
+            TextView textView = new TextView(this);
+            textView.setText("TextView " + String.valueOf(i));
+            setTextViewAttributes(textView);
+            textLinearLayout.addView(textView);
+        }
+        addLineSeperator();
+    }
+
+    private void setTextViewAttributes(TextView textView) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        params.setMargins(convertDpToPixel(16),
+                convertDpToPixel(16),
+                0, 0
+        );
+
+        textView.setTextColor(Color.BLACK);
+        textView.setLayoutParams(params);
+    }
+
+    private int convertDpToPixel (float dp) {
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return Math.round(px);
+    }
+
+    private void addLineSeperator() {
+        LinearLayout lineLayout = new LinearLayout(this);
+        lineLayout.setBackgroundColor(Color.GRAY);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                2);
+        params.setMargins(0, convertDpToPixel(10), 0, convertDpToPixel(10));
+        lineLayout.setLayoutParams(params);
+        lineLayout.addView(lineLayout);
+    }
+
 }
